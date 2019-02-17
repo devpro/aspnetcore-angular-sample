640fc325ba4e86093933b1d4118b44a6e83389e1
Bertrand THOMAS <bertrand@devpro.fr>
2019-02-17 15:50:08 +0100

    Fix one SonarCloud warning on Program static class

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace Devpro.AspNetCoreAngularSample.WebApp
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
